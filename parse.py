b8e1a5fa1e0aa20cd25d908bbdbc29e325063e7c
Petteri Karppinen <petteri@hurja.fi>
2016-06-08 15:20:20 +0300

    added tavoitteet and renamed few functions

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 # encoding=utf8
 import json
+import re
 from pprint import pprint
 import sys
 reload(sys)
@@ -10,7 +11,12 @@ class OpsParser:
 	debug = False
 	grades = {}
 	subjects = []
+	parent_map = []
 	meta = {}
+
+	def debug(self, level, str):
+		print ' ' * (level * 3) + str
+
 
 	def parse_grades(self, items):
 		"""Parse grades"""
@@ -25,37 +31,41 @@ class OpsParser:
 		for item in items:
 			name = item['nimi']['fi']
 			item['name'] = name
-			self.subjects.append(item)  
-			print name
+			self.subjects.append(item) 
+
+			self.debug(0, name)
 
 			# Loop target areas 
 			if "kohdealueet" in item:
+				self.debug(1, "Kohdealueet:")
 				for area in item['kohdealueet']:
-					print (' ' * 3) + area['nimi']['fi']
+					self.debug(2, area['nimi']['fi'])
 
 			# Loop target areas 
 			if "oppimaarat" in item:
+				self.debug(1, "Oppimäärät:")
 				for tmp in item['oppimaarat']:
-					print (' ' * 3) + tmp['nimi']['fi']
+					self.debug(2, tmp['nimi']['fi'])
 
 			# Get grades material 
 			if 'vuosiluokkakokonaisuudet' in item:
 				for info in item['vuosiluokkakokonaisuudet']:
 
 					# Print target grades
-					print (' ' * 3) + self.grades[int(info['_vuosiluokkaKokonaisuus'])]['name']
+					self.debug(2, self.grades[int(info['_vuosiluokkaKokonaisuus'])]['name'])
 			
-					#pprint(pack)
 					if info['sisaltoalueinfo']:
-						print (' ' * 6) + info['sisaltoalueinfo']['otsikko']['fi']
+						self.debug(2, info['sisaltoalueinfo']['otsikko']['fi'])
 
 					# Targets
-					#for target in info['tavoitteet']:
-					#	print (' ' * 6) + target['tavoite']['fi']	
+					self.debug(2, "Tavoitteet:")
+					for target in info['tavoitteet']:
+						self.debug(3, re.sub('<[^<]+?>', '', target['tavoite']['fi']))	
 
 					# Content areas
+					self.debug(2, "Sisältöalueet:")
 					for area in info['sisaltoalueet']:
-						print (' ' * 9) + area['nimi']['fi'] 
+						self.debug(3, area['nimi']['fi'])
 
 
 	def parse_metadata(self, items):
@@ -68,18 +78,24 @@ class OpsParser:
 
 	def parse_tree(self, items, depth=0):
 		for item in items:
-			print (' ' * (depth *3)) + item['perusteenOsa']['nimi']['fi']
+			self.debug(depth, item['perusteenOsa']['nimi']['fi'])
 			if 'lapset' in item:
 				self.parse_tree(item['lapset'], depth+1)
 
 
-	def getGrades(self):
+	def get_subjects(self):
+		"""Returns array of subjects"""	
 		pass
 
 
-	def getSubjectsByGrade(self, id):
+	def get_grades(self):
+		"""Returns array of grades"""	
 		pass
 
+
+	def get_subjects_by_grade(self, id):
+		"""Returns array of subjects"""	
+		pass
 
 
 def main():
